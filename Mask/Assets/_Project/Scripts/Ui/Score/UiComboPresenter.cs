5f0ed4712e5d60d9384afef320c043beeff32012
kr <rynasiewiczk@gmail.com>
2026-01-31 17:04:38 +0100

    add combo bump

--- Mask/Assets/_Project/Scripts/Ui/Score/UiComboPresenter.cs
+++ Mask/Assets/_Project/Scripts/Ui/Score/UiComboPresenter.cs
@@ -1,13 +1,17 @@
 namespace _Project.Scripts.Ui
 {
+    using DG.Tweening;
     using Gameplay.Input.Score;
+    using LazySloth.Utilities.DoTween;
     using TMPro;
     using UnityEngine;
 
     public class UiComboPresenter : MonoBehaviour
     {
-        [SerializeField] private TextMeshProUGUI _comboLabel;
+        [SerializeField] private DOTweenPunchSettings _punchSettings;
         [SerializeField] private TextMeshProUGUI _comboText;
+        
+        private Tween _tween;
 
         private void Start()
         {
@@ -17,20 +21,28 @@ namespace _Project.Scripts.Ui
         private void OnDestroy()
         {
             ScoreManager.Instance.Combo.Unsubscribe(ChangeTexts);
+            _tween?.Kill();
         }
 
         private void ChangeTexts(int combo)
         {
-            if (combo <= 1)
-            {
-                _comboLabel.gameObject.SetActive(false);
-                _comboText.gameObject.SetActive(false);
-                return;
-            }
-            
-            _comboLabel.gameObject.SetActive(true);
-            _comboText.gameObject.SetActive(true);
             _comboText.text = $"x{combo}";
+
+            HandlePunch(combo);
+        }
+
+        [ContextMenu("Update Combo Texts")]
+        private void TextPunch() => HandlePunch(3);
+        
+        private void HandlePunch(int combo)
+        {
+            _tween?.Kill();
+            _comboText.transform.localScale = Vector3.one;
+
+            if (combo > 1)
+            {
+                _tween = _comboText.transform.DOPunchScale(_punchSettings);
+            }
         }
     }
 }
